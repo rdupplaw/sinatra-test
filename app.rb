05bebbadec5c4de5561dae83c8a7d1a45569d7e1
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-10 15:43:55 +0100

    added sinatra/reloader and two test routes

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
 
 get '/' do
   'Hello'
+end
+
+get '/secret' do
+  'this is a secret page'
+end
+
+get '/test' do
+  'this is a test page'
+end
+
+get '/about' do
+  'this is an about page'
 end
