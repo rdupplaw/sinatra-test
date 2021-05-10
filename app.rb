4a3e3c9e9eac9e88f9ecb595ada8a02b7e4ebeb5
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-05-10 16:31:52 +0100

    added name and age params

--- app.rb
+++ app.rb
@@ -17,7 +17,14 @@ get '/about' do
   'this is an about page'
 end
 
-get '/cat' do
+get '/random-cat' do
   @random_cat_name = ["Amigo", "Misty", "Almond"].sample
+  erb(:index)
+end 
+
+get '/named-cat' do
+  p params
+  @random_cat_name = params[:name]
+  @random_cat_age = params[:age]
   erb(:index)
 end 
